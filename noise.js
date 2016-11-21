0b28d2f9e81e39c6d54d0fb599dad8f76ebf9467
Madeleine Johnson <mjohnson3038@gmail.com>
2016-11-21 14:51:57 -0800

    Playing single note on each letter

--- noise.js
+++ noise.js
@@ -1,3 +1,25 @@
 $(document).ready( function() {
-  // your code here
+  $('.instrument').on('click', 'button', function(event) {
+    // var text = $(this).html();
+    // alert("Got a click on an <button> element! with text: " + text);
+    var audio = new Audio('media/e_note.mp3');
+    audio.play();
+    var letter = $(this).html();
+  });
+//
+//   var note = "#" + letter + "Audio";
+    // var toPlay = $(this).html();
+    var audio = new Audio('media/e_note.mp3');
+    audio.play();
+
 });
+
+
+// $(document).ready( function() {
+//   $('button').click(function(event) {
+//     var audio = new Audio('media/e_note.mp3');
+//     audio.play();
+//     var text = $(this).html();
+//     alert("Got a click on an <button> element! with text: " + text);
+//   });
+// });
