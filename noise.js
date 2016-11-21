2dfee95386143fcdc05524192cf39b1a6399304e
Madeleine Johnson <mjohnson3038@gmail.com>
2016-11-21 15:22:36 -0800

    Working with hardcoding mp3

--- noise.js
+++ noise.js
@@ -1,25 +1,11 @@
 $(document).ready( function() {
   $('.instrument').on('click', 'button', function(event) {
-    // var text = $(this).html();
-    // alert("Got a click on an <button> element! with text: " + text);
-    var audio = new Audio('media/e_note.mp3');
+    var text = $(this).html();
+    var note = "media/" + text + "_note.mp3";
+    var audio = new Audio(note);
     audio.play();
-    var letter = $(this).html();
   });
-//
-//   var note = "#" + letter + "Audio";
-    // var toPlay = $(this).html();
-    var audio = new Audio('media/e_note.mp3');
-    audio.play();
+
+
 
 });
-
-
-// $(document).ready( function() {
-//   $('button').click(function(event) {
-//     var audio = new Audio('media/e_note.mp3');
-//     audio.play();
-//     var text = $(this).html();
-//     alert("Got a click on an <button> element! with text: " + text);
-//   });
-// });
