ab8e7dfc792b442b5d80ca8dc9b779c7a44b5031
Madeleine Johnson <mjohnson3038@gmail.com>
2016-11-21 16:50:08 -0800

    final touches

--- noise.js
+++ noise.js
@@ -1,7 +1,6 @@
 $(document).ready( function() {
   $('.instrument').on('click', 'button', function(event) {
     var text = $(this).html();
-    var note = "media/" + text + "_note.mp3";
     var audioId = text + "Audio";
     var audio = document.getElementById(audioId);
     audio.load();
@@ -10,11 +9,13 @@ $(document).ready( function() {
   });
 
 
-  $('.instrument').keydown(function(e) {
+  // var audio = new Audio ("media/g_note.mp3")
+
+  $(document).keydown(function(e) {
+    console.log(e);
     var key = e.key;
     var audioId =  key + "Audio";
     var audio = document.getElementById(audioId);
-    audio.play();
     audio.load();
     audio.play();
     console.log(audio);
